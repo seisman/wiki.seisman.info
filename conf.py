3312ac0426d11078f4381a7dbefe1e8859fbf79c
Dongdong Tian <seisman.info@gmail.com>
2019-01-25 00:29:06 -0500

    Add CNAME

--- conf.py
+++ conf.py
@@ -69,6 +69,8 @@ language = None
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+html_extra_path = ['CNAME']
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
