2194e00fba4696c25012d0b41ce6ba714e6e3b3e
Dongdong Tian <seisman.info@gmail.com>
2019-04-30 10:58:40 -0400

    Remove latex output

--- conf.py
+++ conf.py
@@ -105,34 +105,6 @@ html_static_path = ['_static']
 # html_sidebars = {}
 html_last_updated_fmt = '%b %d, %Y'
 
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'SeisMansWiki.tex', 'SeisMan\'s Wiki Documentation',
-     'Dongdong Tian', 'manual'),
-]
-
 # -- Extension configuration -------------------------------------------------
 
 # -- Options for todo extension ----------------------------------------------
